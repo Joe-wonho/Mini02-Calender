e71cb27693e55f2ac5857d0d654718fb258a9ea4
Joe-wonho <whdnjsgh22@naver.com>
2021-10-13 00:23:37 +0900

    cors 추가

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-require("dotenv").config();
+const express = require('express');
 const app = express();
 const port = 3000;
-
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const cors = require('cors');
 // MongoDB Router
-const connect = require("./models");
+const connect = require('./models');
 connect();
 
 // Middleware
-app.set("views", __dirname + "/views");
-app.set("view engine", "ejs");
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser(process.env.SECRET_KEY));
+app.use(cors({}));
 
 // Router
-const loginUpPageRouter = require("./routers/login");
-app.use("/login", loginUpPageRouter);
-const signUpPageRouter = require("./routers/signup");
-app.use("/signup", signUpPageRouter);
-const mainPageRouter = require("./routers");
-app.use("/", mainPageRouter);
-const diaryRouter = require("./routers/diary");
-app.use("/diary", diaryRouter);
+const loginUpPageRouter = require('./routers/login');
+app.use('/login', loginUpPageRouter);
+const signUpPageRouter = require('./routers/signup');
+app.use('/signup', signUpPageRouter);
+const mainPageRouter = require('./routers');
+app.use('/', mainPageRouter);
+const diaryRouter = require('./routers/diary');
+app.use('/diary', diaryRouter);
 
 //Render
 
@@ -32,15 +33,15 @@ app.use("/diary", diaryRouter);
 //   res.render("index");
 // });
 
-//로그인 페이지
-app.get("/login", (req, res) => {
-  res.render("login");
-});
+// //로그인 페이지
+// app.get('/login', (req, res) => {
+//   res.render('login');
+// });
 
-//회원가입페이지
-app.get("/signup", (req, res) => {
-  res.render("signup");
-});
+// //회원가입페이지
+// app.get('/signup', (req, res) => {
+//   res.render('signup');
+// });
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:3000`);
